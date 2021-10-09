86dad9f1de2c0fafe48c4097c8ddbff62f88a734
Mark cabanes <cabanesdev@gmail.com>
2021-10-09 19:51:18 +0200

    add anime container

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', ($scope, $http) => {
-	$http
-		.get('https://kitsu.io/api/edge/anime?filter[text]=sao')
-		.then((response) => {
-			let data = response.data.data;
-		});
+	$scope.prueba = () => {
+		let randomRange = Math.floor(Math.random() * 17050);
+		$http
+			.get(
+				`https://kitsu.io/api/edge/anime?page[limit]=20&page[offset]=${randomRange}`
+			)
+			.then((response) => {
+				let data = response.data.data;
+				$scope.animes = data;
+			});
+	};
+
+	$scope.prueba();
+
+	$scope.getAnimes = () => {
+		console.log('hola');
+	};
 });
