bc346c1384e29d3868507aeec613572acc66cccc
Mark cabanes <cabanesdev@gmail.com>
2021-10-09 22:55:44 +0200

    fix searcher bug

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', ($scope, $http) => {
 	$scope.prueba = () => {
-		let randomRange = Math.floor(Math.random() * 17050);
+		const randomRange = Math.floor(Math.random() * 17050);
 		$http
 			.get(
 				`https://kitsu.io/api/edge/anime?page[limit]=20&page[offset]=${randomRange}`
 			)
 			.then((response) => {
-				let data = response.data.data;
+				const data = response.data.data;
 				$scope.animes = data;
 			});
 	};
@@ -16,13 +16,12 @@ app.controller('myCtrl', ($scope, $http) => {
 
 	$scope.getAnimes = (input) => {
 		$scope.input = input.toLowerCase();
-		$http
-			.get(
-				`https://kitsu.io/api/edge/anime?filter[text]=${input}&page[limit]=20`
-			)
-			.then((response) => {
-				let data = response.data.data;
-				$scope.animes = data;
-			});
+		let url = 'https://kitsu.io/api/edge/anime?page[limit]=20';
+		if (!$scope.input)
+			url = `https://kitsu.io/api/edge/anime?filter[text]=${input}&page[limit]=20`;
+		$http.get(url).then((response) => {
+			const data = response.data.data;
+			$scope.animes = data;
+		});
 	};
 });
